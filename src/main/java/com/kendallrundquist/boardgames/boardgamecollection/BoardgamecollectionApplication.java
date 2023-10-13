f113b9c435a72c9e4ca96a55a0aeab6cf9c02f2e
Kendall Rundquist <Tythyrn@users.noreply.github.com>
2023-10-12 20:36:17 -0500

    Initial commit

---
+++ src/main/java/com/kendallrundquist/boardgames/boardgamecollection/BoardgamecollectionApplication.java
@@ -0,0 +1,13 @@
+package com.kendallrundquist.boardgames.boardgamecollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardgamecollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardgamecollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kendallrundquist/boardgames/boardgamecollection/BoardgamecollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kendallrundquist.boardgames.boardgamecollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardgamecollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
